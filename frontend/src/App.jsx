e9cdab9f78ffb7abdced797ab18251d90a9fed98
Hajer Bouricha <hajer.bouricha@ensi-uma.tn>
2025-03-05 22:57:11 +0100

    add routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,24 @@
-import React from 'react'
+
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import LoginForm from './pages/Auth/LoginForm';
+import SingUpForm from './pages/Auth/SingUpForm';
+import Expense from './pages/Dashboard/Expense';
+import Income from './pages/Dashboard/Income';
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Rout />} />
+          <Route path="/login" element={<LoginForm />} />
+          <Route path="/signUp" element={<SingUpForm />} />
+          <Route path="/dashboard" element={<Home />} />
+          <Route path="/income" element={<Expense />} />
+          <Route path="/expense" element={<Income />} />
+        </Routes>
+      </Router>
+    </div>
   )
 }
 
